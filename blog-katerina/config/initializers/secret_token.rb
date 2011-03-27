366ae8496c2d7ad37b293bedfd401eacbffa736b
Katerina Markova <katerina@infochimps.com>
2011-03-27 16:25:20 -0500

    readme blog-katerina update

---
+++ blog-katerina/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlogKaterina::Application.config.secret_token = 'b1c6f17c2811c5d56005334492cfd2aa1242ba7637db86b314085944431978fc0040ed2f163e77c6f6784650894fbbd90396e7b227d3a7efae0136997b256db7'
