71a10210076802fb7e93114f95273c0767654677
Katerina Markova <katerina@infochimps.com>
2011-03-27 16:11:25 -0500

    changes to the structure of the folder

---
+++ testApp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestApp::Application.config.secret_token = 'b3142a4c6283ca6aa6d02e6041cf36d3e16d206b1917c072150801b7724f010988b81a331be9c6d1e5308a42b32f41a76d0e8dde8bd1ad5f1344b8a3b225e12b'
